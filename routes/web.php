24fdf2a57e972ba05ca5a73ab23213be22942078
Botir <tiboruz@gmail.com>
2024-11-21 12:51:47 +0500

    user route

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use App\Http\Controllers\AdinesController;
 */
 
 Route::get('/', function () {
-    return view('index',['title'=>'Dashboard']);
+    return view('index', ['title' => 'Dashboard']);
 })->middleware('auth')->name('main');
 
 Auth::routes();
@@ -28,7 +28,7 @@ Auth::routes();
 Route::get('/home', [PriceController::class, 'index'])->name('home');
 
 
-Route::group(['middleware'=>'auth','prefix'=>'price','as'=>'price.'],function(){
+Route::group(['middleware' => 'auth', 'prefix' => 'price', 'as' => 'price.'], function () {
 
     Route::get('', [PriceController::class, 'index'])->name('index');
 
@@ -38,63 +38,62 @@ Route::group(['middleware'=>'auth','prefix'=>'price','as'=>'price.'],function(){
 
     Route::get('/dellitem', [PriceController::class, 'dellitem'])->name('dell.item');
 
-    Route::get('/load1c',[PriceController::class, 'load1c'])->name('load1c');
+    Route::get('/load1c', [PriceController::class, 'load1c'])->name('load1c');
 });
 
 
-Route::group(['middleware'=>'auth','prefix'=>'orders','as'=>'orders.'],function(){
-    
-    Route::get('',[OrderController::class,'orders'])->name('index');
+Route::group(['middleware' => 'auth', 'prefix' => 'orders', 'as' => 'orders.'], function () {
 
-    Route::get('/create',[OrderController::class,'orderCreate'])->name('create');
+    Route::get('', [OrderController::class, 'orders'])->name('index');
 
-    Route::get('/ready',[OrderController::class,'readyOrders'])->name('ready');
+    Route::get('/create', [OrderController::class, 'orderCreate'])->name('create');
 
-    
-    Route::get('/edit/item/{id}',[OrderController::class,'orderEditItem'])->name('edit.item');
+    Route::get('/ready', [OrderController::class, 'readyOrders'])->name('ready');
 
-    Route::get('/{id}',[OrderController::class,'order'])->name('show');
 
-    Route::get('/{id}/delete',[OrderController::class,'orderDelete'])->name('delete');
+    Route::get('/edit/item/{id}', [OrderController::class, 'orderEditItem'])->name('edit.item');
 
-    Route::post('/{id}/item',[OrderController::class,'orderItem'])->name('item');
+    Route::get('/{id}', [OrderController::class, 'order'])->name('show');
 
-    Route::post('/{id}/add/item',[OrderController::class,'orderAddItem'])->name('add.item');
+    Route::get('/{id}/delete', [OrderController::class, 'orderDelete'])->name('delete');
 
-    Route::post('/{id}/check/item',[OrderController::class,'checkItem'])->name('check.item');
+    Route::post('/{id}/item', [OrderController::class, 'orderItem'])->name('item');
 
-    Route::post('/{id}/addCollectItem',[OrderController::class,'collectAddItem'])->name('collect.add.Item');
+    Route::post('/{id}/add/item', [OrderController::class, 'orderAddItem'])->name('add.item');
 
-    
+    Route::post('/{id}/check/item', [OrderController::class, 'checkItem'])->name('check.item');
 
-    Route::get('/{id}/status/{status}',[OrderController::class,'orderStatus'])->name('status');
+    Route::post('/{id}/addCollectItem', [OrderController::class, 'collectAddItem'])->name('collect.add.Item');
 
-    Route::get('/{id}/delete/{item}',[OrderController::class,'orderDeleteItem'])->name('delete.item');
 
-    
+
+    Route::get('/{id}/status/{status}', [OrderController::class, 'orderStatus'])->name('status');
+
+    Route::get('/{id}/delete/{item}', [OrderController::class, 'orderDeleteItem'])->name('delete.item');
 });
 
 
+Route::group(['middleware' => 'auth', 'prefix' => 'users', 'as' => 'users.'], function () {
+    
+    Route::get('', [UserController::class, 'index'])->name('index');
 
-Route::get('/users',[UserController::class,'index'])->name('users.index');
+    Route::get('/add', [UserController::class, 'add'])->name('add');
 
-Route::get('/users/add',[UserController::class,'add'])->name('users.add');
+    Route::post('/add', [UserController::class, 'store'])->name('store');
 
-Route::get('/user/{id}',[UserController::class,'show'])->name('users.show');
+    Route::get('/{id}', [UserController::class, 'show'])->name('show');
 
-Route::get('/user/{id}/edit',[UserController::class,'edit'])->name('users.edit');
+    Route::get('/{id}/edit', [UserController::class, 'edit'])->name('edit');
 
-Route::post('/user/add',[UserController::class,'store'])->name('users.store');
+   
+});
 
+Route::get('/shops', [ShopController::class, 'index'])->name('shops.index');
 
+Route::get('/shops/update', [ShopController::class, 'update'])->name('shops.update');
 
-
-Route::get('/shops',[ShopController::class,'index'])->name('shops.index');
-
-Route::get('/shops/update',[ShopController::class,'update'])->name('shops.update');
-
-Route::get('/adines',[AdinesController::class,'index'])->name('adines.index');
+Route::get('/adines', [AdinesController::class, 'index'])->name('adines.index');
 
 //Route::get('/monitoring',[MonitoringController::class,'index'])->name('monitoring');
 
-Route::get('/logout',[PriceController::class,'logout'])->name('logout');
+Route::get('/logout', [PriceController::class, 'logout'])->name('logout');
