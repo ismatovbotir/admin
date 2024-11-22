b08670fae5412da91111e09a434b74dabf9fecf1
Botir <tiboruz@gmail.com>
2024-11-22 17:07:26 +0500

    sdfsfasfs

--- routes/web.php
+++ routes/web.php
@@ -75,7 +75,7 @@ Route::group(['middleware' => 'auth', 'prefix' => 'collects', 'as' => 'collects.
 
     Route::get('/{id}', [CollectController::class, 'collect'])->name('show');
 
-    Route::get('item/{id}/edit',[CollectController::class,'editItem'])->name('edit.item');
+    Route::get('item/{id}/edit',[CollectController::class,'collectEditItem'])->name('edit.item');
 
     Route::post('/{id}/check/item', [CollectController::class, 'checkItem'])->name('check.item');
 
