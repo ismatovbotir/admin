0119c8bd55362fb22143e9ce40b4af27c7f3098e
Botir <tiboruz@gmail.com>
2024-11-25 13:13:13 +0500

    commit fistr today 25112024

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -38,4 +38,7 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
         $this->middleware('auth')->only('logout');
     }
+    public function username(){
+        return 'email';
+    }
 }
